d44adc2d82846f9cebf552805ef56bb27fe16b1b
Stephane Thorez <stephane.thorez@radio-canada.ca>
2025-02-18 10:57:25 -0500

    [IOSINFO-11551] Add support for SPM

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# DRPLib
+
+# Version 4.0.0
+February 12, 2025
+Author: stephane thorez
+- Add support for SPM
+
---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "HWToast",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "HWToast",
+            targets: ["HWToast"]),
+    ],
+    targets: [
+        .target(
+            name: "HWToast",
+            resources: [.process("HWToastImage.bundle")],
+            cSettings: [
+                .headerSearchPath("Sources/**/*.{h,m}")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
